aa3132f3e647c21b9162aef54b6ef20c6fa761e5
David Oliveira <david@Davids-MacBook.local>
2016-09-29 00:08:38 +0100

    WIP

--- strace_simple.pl
+++ strace_simple.pl
@@ -3,13 +3,25 @@
 use strict;
 use warnings;
 
+# TODO:
+#
+# - Support for <unfinished ...> and <... bla resumed>
+#
+
+
+# Parse a system call argument list
+sub parseArgs {
+	my ($argStr) = @_;
+#	print "PARSE: $argStr\n";
+}
 
 # Register a system call
 sub registerCall {
 	my ($pid, $time, $call) = @_;
 
-	if ( $call =~ /^\s*(\w+)\(/ ) {
-		my $syscall = $1;
+	if ( $call =~ /^\s*(\w+)\((.*?)\) += +(\-?[\dxa-f]+|\?)/ ) {
+		my ($syscall, $argStr) = ($1, $2);
+		my @args = parseArgs($argStr);
 		if ( $syscall eq "connect" ) {
 			print "IM CONNECTING!! $call\n";
 		}
@@ -18,11 +30,20 @@ sub registerCall {
 
 }
 
+# Register a process exit
+sub registerExit {
+	my ($pid, $time, $signal) = @_;
+}
+
+# Register a received signal
+sub registerSignal {
+	my ($pid, $time, $signal, $data) = @_;
+}
 
 my ($source, $arg) = @ARGV;
 $source || die "Please specify an source type (pid|exec)\n";
 
-my $strace = "strace -s 2000 -f -t ";
+my $strace = "strace -s 4096 -q -f -tt ";
 if ( $source eq "pid" ) {
 	$arg || die "Please specify a process id\n";
 	$strace = "-p $arg 2>&1 |";
@@ -48,13 +69,13 @@ while (<STRACE>) {
 	$pid = $1 if !$pid && s/^\s*\[pid +(\d+)\] +//;
 
 	# Get the time
-	$time = $1 if !$time && s/^\s*(\d+:\d+:\d+) +//;
+	$time = $1 if !$time && s/^\s*(\d{1,2}:\d{1,2}:\d{1,2}(?:\.\d{6})?) +//;
 
 	# The rest of the line
 	my $call = ($prev ? $prev->{data} : "") . $_;
 
 	# Does it continue ?
-	$continues = !/\s+=\s+(\-?[\dxa-f]+)\s*(?: +[A-Z]+ +\([\w.\- ]+\)\s*)?$/;
+	$continues = !/\s+=\s+(\-?[\dxa-f]+|\?)\s*(?:(?:[A-Z_]+ +)?\([^)]*?\))?$/;
 	if ( !$continues ) {
 		$pid ||= "main";
 		$time ||= "??";
@@ -63,7 +84,16 @@ while (<STRACE>) {
 		$prev = undef;
 	}
 	else {
-#		print "CONT: $_\n";
+		if ( /^\s*\+{3}\s+exited with\s*(\d+)\s+\+{3}\s*$/ ) {
+			registerExit($pid, $time, $1);
+			next;
+		}
+		elsif ( /^\s*\-{3}\s+([A-Z]+)(?: (\{.+?\}))?\s+\-{3}\s*$/ ) {
+			registerSignal($pid, $time, $1, $2);
+			next;
+		}
+
+		print "CONT: $_\n";
 		if ( $prev ) {
 			$prev->{data} .= $_;
 		}
